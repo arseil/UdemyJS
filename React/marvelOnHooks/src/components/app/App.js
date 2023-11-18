851cec4affd594fc61a844c52388f9c24e49b655
arseil <arceil7337@gmail.com>
2023-11-18 13:52:27 +0300

    init: marvel project on hooks

--- React/marvelOnHooks/src/components/app/App.js
+++ React/marvelOnHooks/src/components/app/App.js
@@ -9,6 +9,7 @@ const MainPage = lazy(() => import('../pages/MainPage'));
 const ComicsPage = lazy(() => import('../pages/ComicsPage'));
 const SingleComicPage = lazy(() => import('../pages/SingleComicPage'));
 
+
 const App = () => {
 
     return (
